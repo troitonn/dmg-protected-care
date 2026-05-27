458367addbcbed9d6e93316328684b1189c00aa0
troitonn <150198515+troitonn@users.noreply.github.com>
2026-05-27 15:20:35 -0300

    Update sitemap[.]xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 import { supabaseAdmin } from "@/integrations/supabase/client.server";
 
-const BASE_URL = "https://dmg-protected-care.lovable.app";
+const BASE_URL = "https://dmgocupacional.com";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
